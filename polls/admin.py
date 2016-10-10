c4701f1b4a81bdfd629687c270d697cffb72c515
safaghribi <safa.ghribi@depinfonancy.net>
2016-10-11 01:45:47 +0200

    la repository polls

--- polls/admin.py
+++ polls/admin.py
@@ -13,10 +13,11 @@ class QuestionAdmin(admin.ModelAdmin):
             ('Date information', {'fields':['pub_date'],'classes': ['collapse']}),
             ]
     inlines = [ChoiceInline]
-    list_display = ('question_test','pub_date','was_published_recently')
+    #list_display = ('question_test','pub_date','was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
 
 # Register your models here.
